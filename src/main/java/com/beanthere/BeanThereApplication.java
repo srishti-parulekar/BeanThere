33004e7cf978841f4bff5671ce2702e959939b8a
Srishti Parulekar <srishtiparulekar430@gmail.com>
2024-09-07 11:23:40 +0530

    setting up

---
+++ src/main/java/com/beanthere/BeanThereApplication.java
@@ -0,0 +1,13 @@
+package com.beanthere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanThereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanThereApplication.class, args);
+    }
+
+}
